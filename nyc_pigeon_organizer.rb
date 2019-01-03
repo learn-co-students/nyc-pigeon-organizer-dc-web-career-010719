f75f1142a6378c7fb75b3fc46c459f73b942291a
Anthony Gregg <agregg22@gmail.com>
2019-01-02 22:44:15 -0500

    Complete

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -35,20 +35,20 @@ def nyc_pigeon_organizer(data)
   # binging.pry
 end
 
-Better way
-
-def nyc_pigeon_organizer(data)
-
-  pigeon_list = {}
-  data.each do |trait,specifics|
-    specifics.each do |element,name_array|
-      name_array.each do |name|
-        pigeon_list[name] ||= {}
-        pigeon_list[name][trait] ||= []
-        pigeon_list[name][trait] << element.to_s
-      end
-    end
-  end
-  return pigeon_list
-  # binging.pry
-end
+# Better way
+#
+# def nyc_pigeon_organizer(data)
+#
+#   pigeon_list = {}
+#   data.each do |trait,specifics|
+#     specifics.each do |element,name_array|
+#       name_array.each do |name|
+#         pigeon_list[name] ||= {}
+#         pigeon_list[name][trait] ||= []
+#         pigeon_list[name][trait] << element.to_s
+#       end
+#     end
+#   end
+#   return pigeon_list
+#   # binging.pry
+# end

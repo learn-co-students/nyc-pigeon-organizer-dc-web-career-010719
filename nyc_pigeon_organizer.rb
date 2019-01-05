9072c7f9322b4f4185074db63b4f65ae755a7ebf
shannon-nabors <shannon.nabors@gmail.com>
2019-01-04 19:00:42 -0500

    Done

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,23 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeons = []
+  data.values.each do |hash|
+    pigeons << hash.values
+  end
+  pigeon_list = {}
+  pigeons.flatten.uniq.each do |pigeon|
+    trait_hash = {}
+    data.each do |trait|
+      adj_array = []
+      trait[1].each do |adj|
+        if adj[1].include?(pigeon)
+          adj_array << adj[0].to_s
+        end
+        trait_hash[trait[0]] = adj_array
+      end
+    end
+    pigeon_list[pigeon] = trait_hash
+  end
+  pigeon_list
 end
